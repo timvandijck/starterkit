31dfa257d8bacace15826a26d9f2fb7c404d0007
Tim Van Dijck <tim.van.dijck@gmail.com>
2016-04-06 10:50:19 +0200

    Initial commit.

---
+++ gulp_tasks/sass.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+     gulp.src('./app/sass/screen.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/assets/css'));
+});
---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('default', ['connect']);
+
